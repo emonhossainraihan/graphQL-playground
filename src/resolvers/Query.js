c134de4e34376b3ba61126fe58e28ceaa59d506e
EmonHR <mdemon7475@gmail.com>
2020-09-15 22:11:42 +0600

    fix typo

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,10 @@
 function feed(parent, args, context) {
     return context.prisma.link.findMany();
 }
-
+function info(parent) {
+    return 'Hello buddy, This GraphQL playground is created by Emon Hossain'
+}
 module.exports = {
-    feed
+    feed,
+    info
 }
